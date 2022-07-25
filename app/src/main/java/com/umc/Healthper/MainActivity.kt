c64d8beb6331b9a0c4b988aad784af48cd7a3555
minhee020 <minhee020@naver.com>
2022-07-25 21:34:53 +0900

    favorite, music

--- app/src/main/java/com/umc/Healthper/MainActivity.kt
+++ app/src/main/java/com/umc/Healthper/MainActivity.kt
@@ -12,8 +12,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.main_frm, PartchartFragment())
+//            .commit()
+
         supportFragmentManager.beginTransaction()
-            .replace(R.id.main_frm, PartchartFragment())
+            .replace(R.id.main_frm, MypageFragment())
             .commit()
+    }
+
+    fun changeMypageFragment(int : Int){
+        val transition = supportFragmentManager.beginTransaction()
+        when (int){
+            0 -> transition.replace(R.id.main_frm, FavoritesMypageFragment())
+            1 -> transition.replace(R.id.main_frm, MusicMypageFragment())
+        }
+        transition.commit()
     }
 }
