78ef4a7cf440aa7463e6f2b0c292e1fa9f2989f2
minhee020 <minhee020@naver.com>
2022-07-25 19:12:18 +0900

    backup

--- app/src/main/java/com/umc/Healthper/MainActivity.kt
+++ app/src/main/java/com/umc/Healthper/MainActivity.kt
@@ -15,6 +15,5 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_frm, PartchartFragment())
             .commit()
-
     }
 }
