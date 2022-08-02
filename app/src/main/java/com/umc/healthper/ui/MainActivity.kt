730188ef502339a31630c2093369ca37f75b3cd5
minhee020 <minhee020@naver.com>
2022-08-02 20:49:22 +0900

    error) Global, add kakao login

--- app/src/main/java/com/umc/healthper/ui/MainActivity.kt
+++ app/src/main/java/com/umc/healthper/ui/MainActivity.kt
@@ -3,8 +3,10 @@ package com.umc.healthper.ui
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.kakao.sdk.common.util.Utility
 import com.umc.healthper.R
 import com.umc.healthper.databinding.ActivityMainBinding
 import com.umc.healthper.ui.chart.view.ChartFragment
@@ -28,6 +30,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+//        val keyHash = Utility.getKeyHash(this)//onCreate 안에 입력해주자
+//        Log.d("Hash", keyHash)
 
         VarUtil.glob.mainContext = applicationContext
 
