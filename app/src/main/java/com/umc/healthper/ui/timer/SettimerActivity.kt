796e9f205be9d35d949025eb13edfc3b51ea32c8
minhee020 <minhee020@naver.com>
2022-08-03 18:46:41 +0900

    set2timer

--- app/src/main/java/com/umc/healthper/ui/timer/SettimerActivity.kt
+++ app/src/main/java/com/umc/healthper/ui/timer/SettimerActivity.kt
@@ -2,8 +2,10 @@ package com.umc.healthper.ui.timer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 import com.umc.healthper.R
 import com.umc.healthper.databinding.ActivitySetTimerBinding
+import com.umc.healthper.ui.main.view.WorkReadyFragment
 
 class SettimerActivity : AppCompatActivity() {
     lateinit var binding: ActivitySetTimerBinding
@@ -13,11 +15,15 @@ class SettimerActivity : AppCompatActivity() {
         binding = ActivitySetTimerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.setTimerWorkstartBt.setOnClickListener{
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.main_layout, TimerFragment())
-                .commit()
-        }
+        supportFragmentManager.beginTransaction().add(R.id.main_cl, SettimerFragment()).commit()
+    }
+
+    fun changeTimerFragment() {
+        val trans = supportFragmentManager.beginTransaction()
+
+        trans.replace(binding.mainCl.id, TimerFragment()).addToBackStack("timerFragment")
+        trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        trans.isAddToBackStackAllowed
+        trans.commit()
     }
 }
---
+++ app/src/main/java/com/umc/healthper/ui/timer/SettimerFragment.kt
@@ -0,0 +1,34 @@
+package com.umc.healthper.ui.timer
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.umc.healthper.databinding.FragmentSetTimerBinding
+import com.umc.healthper.ui.main.view.WorkReadyFragment
+
+class SettimerFragment : Fragment() {
+    lateinit var binding : FragmentSetTimerBinding
+    var settimerActivity: SettimerActivity? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        settimerActivity = context as SettimerActivity
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSetTimerBinding.inflate(inflater, container, false)
+
+        binding.setTimerWorkstartBt.setOnClickListener{
+            settimerActivity!!.changeTimerFragment()
+        }
+        return binding.root
+    }
+}
