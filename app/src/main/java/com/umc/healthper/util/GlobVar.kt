02de068c20878f56eafacc99fc4047aa32841884
Choi <csw3055@gmail.com>
2022-08-04 12:20:10 +0900

    add drawerLayout

--- app/src/main/java/com/umc/healthper/util/GlobVar.kt
+++ app/src/main/java/com/umc/healthper/util/GlobVar.kt
@@ -3,8 +3,10 @@ package com.umc.healthper.util
 import android.app.Application
 import android.content.Context
 import android.widget.TextView
+import com.umc.healthper.ui.MainActivity
 
 class GlobVar(): Application() {
     lateinit var mainContext: Context
+    lateinit var mainActivity: MainActivity
     lateinit var today: TextView
 }
