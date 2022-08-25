724bee5d52ae0c591b910ce27b89256bea6fe4ba
minhee020 <minhee020@naver.com>
2022-08-25 20:28:08 +0900

    myPage

--- app/src/main/java/com/umc/healthper/ui/board/view/MyboardBoard.kt
+++ app/src/main/java/com/umc/healthper/ui/board/view/MyboardBoard.kt
@@ -1,6 +1,10 @@
 package com.umc.healthper.ui.board
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.umc.healthper.databinding.FragmentBoardMyboardBinding
 import com.umc.healthper.ui.MainActivity
@@ -12,5 +16,15 @@ class MyboardBoardFragment : Fragment() {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         mainActivity = context as MainActivity
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBoardMyboardBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 }
