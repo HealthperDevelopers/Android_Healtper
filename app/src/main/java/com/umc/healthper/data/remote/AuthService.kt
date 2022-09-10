8b0a3cb307cd0542ed925c5529342e1e41988c42
minhee020 <minhee020@naver.com>
2022-09-10 10:57:12 +0900

    boardFragment

--- app/src/main/java/com/umc/healthper/data/remote/AuthService.kt
+++ app/src/main/java/com/umc/healthper/data/remote/AuthService.kt
@@ -240,6 +240,7 @@ class AuthService {
                     }
                     else -> {
                         Log.d("deleteComment/fail", response.body().toString())
+                        Toast.makeText(VarUtil.glob.mainContext, "댓글을 수정/삭제할 수 있는 권한이 없습니다.", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
